0a1133fa55ec3b8a1f347caaf0eff35ebda4b4d1
Jason Bouchard <jason@dypabo.com>
2025-01-24 00:04:18 -0500

    build(ruff): enable simplify rule. fix warnings

--- tests/system/utilities_test.py
+++ tests/system/utilities_test.py
@@ -2,6 +2,9 @@ from tests.system.utilities import get_webpage_title
 
 
 def test_get_title_extract() -> None:
-    assert "TITLE" == get_webpage_title(
-        """<html><title>TITLE</title><body>BODY</body></html>""",
+    assert (
+        get_webpage_title(
+            """<html><title>TITLE</title><body>BODY</body></html>""",
+        )
+        == "TITLE"
     )
