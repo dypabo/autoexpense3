fcecddc7eeae9554f8413fbfa10901fd87a62f79
Jason Bouchard <jason@dypabo.com>
2025-01-24 00:46:48 -0500

    build(mypy): add config and missing type dependencies

--- tests/unit/web_app/builder_test.py
+++ tests/unit/web_app/builder_test.py
@@ -21,4 +21,4 @@ def client(app: FastAPI) -> TestClient:
 def test_app_name_in_homepage_title(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == requests.codes.ok
-    assert APP_NAME in get_webpage_title(response.content)
+    assert APP_NAME in get_webpage_title(str(response.content))
