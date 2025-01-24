ecc608456ed8a2c69248c470c45e154d128e9aed
Jason Bouchard <jason@dypabo.com>
2025-01-24 00:02:37 -0500

    build(ruff): enable pytest rule. fix warnings

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -1,9 +1,9 @@
+import pytest
 import requests
-from pytest import fixture
 
 from autoexpense3.web_app.constants import APP_URL
 
 
-@fixture
+@pytest.fixture
 def homepage_content() -> str:
     return requests.get(APP_URL, timeout=3).text
--- tests/unit/web_app/builder_test.py
+++ tests/unit/web_app/builder_test.py
@@ -1,18 +1,18 @@
+import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from pytest import fixture
 
 from autoexpense3.web_app.app_builder import builder
 from autoexpense3.web_app.constants import APP_NAME
 from tests.system.utilities import get_webpage_title
 
 
-@fixture
+@pytest.fixture
 def app() -> FastAPI:
     return builder()
 
 
-@fixture
+@pytest.fixture
 def client(app: FastAPI) -> TestClient:
     return TestClient(app)
 
