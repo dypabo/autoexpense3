c6ebfb553853ed5bad53a7b7a679add263d0a1ef
Jason Bouchard <jason@dypabo.com>
2025-01-25 00:29:52 -0500

    refactor: move homepage variable to jinja template

--- autoexpense3/web_app/app_builder.py
+++ autoexpense3/web_app/app_builder.py
@@ -1,22 +1,27 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader
 
 from autoexpense3.web_app.constants import APP_NAME
+
+environment = Environment(
+    loader=FileSystemLoader("autoexpense3/web_app/templates"),
+    autoescape=True,
+)
+
+
+def homepage(request: Request) -> HTMLResponse:
+    """Homepage route request processor."""
+    _ = request
+    homepage_template = environment.get_template("homepage.html")
+    rendered_template = homepage_template.render(app_name=APP_NAME)
+    return HTMLResponse(rendered_template)
 
 
 def builder() -> FastAPI:
     """Return a FastAPI application."""
     app = FastAPI()
-
-    @app.get("/")
-    def homepage() -> HTMLResponse:
-        return HTMLResponse(
-            f"""
-            <html>
-            <title>{APP_NAME}</title>
-            <body>Under construction</body>
-            </html>
-            """,
-        )
+    app.add_route("/", homepage, methods=["get"])
 
     return app
