0094a3442881ee8ddb9368249cfcb033e0730b83
Jason Bouchard <jason@dypabo.com>
2025-01-31 20:54:48 -0500

    test: add new expense form is present

---
+++ tests/system/add_expense_test.py
@@ -0,0 +1,13 @@
+import pytest
+from playwright.sync_api import Page
+from playwright.sync_api import expect
+
+SELLER = "new_expense_seller"
+DATE = "new_expense_date"
+TOTAL = "new_expense_total"
+BUTTON = "new_expense_button"
+
+
+@pytest.mark.parametrize("field", [SELLER, DATE, TOTAL, BUTTON])
+def test_page_has_new_expense_form(homepage: Page, field: str) -> None:
+    expect(homepage.get_by_placeholder(field)).to_be_visible(timeout=0.1)
