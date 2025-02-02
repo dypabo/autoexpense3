f685a99665368ef65c161487810c3b0b7acf4233
Jason Bouchard <jason@dypabo.com>
2025-02-02 17:04:20 -0500

    refactor(web_app): use HTMX

--- autoexpense3/models/expense.py
+++ autoexpense3/models/expense.py
@@ -16,3 +16,11 @@ class Expense:
     # TODO(Jason): replace py proper type for money
     # https://github.com/dypabo/autoexpense3/issues/1
     total: float
+
+    def jsonify(self) -> dict:
+        """Return a JSON representation of this instance."""
+        return {
+            "timestamp": str(self.timestamp),
+            "seller": self.seller,
+            "total": self.total,
+        }
--- autoexpense3/web_app/app_builder.py
+++ autoexpense3/web_app/app_builder.py
@@ -4,9 +4,12 @@ from typing import Annotated
 
 from fastapi import FastAPI
 from fastapi import Form
+from fastapi import Header
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.responses import JSONResponse
+from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 
@@ -23,26 +26,39 @@ environment = Environment(
 )
 
 app = FastAPI()
+templates = Jinja2Templates(directory="autoexpense3/web_app/templates")
+
+
+@app.get("/expenses")
+def expenses(hx_request: Annotated[str | None, Header] = None) -> Response:
+    """Get a list of expenses from application repository."""
+    if application is None:
+        return JSONResponse("")
+    if hx_request:
+        return HTMLResponse("")
+    return JSONResponse([e.jsonify() for e in application.repository.get_expenses()])
 
 
 @app.post("/new_expense")
 def new_expense(
-    new_expense_date: Annotated[str, Form],
-    new_expense_seller: Annotated[str, Form],
-    new_expense_total: Annotated[float, Form],
-) -> JSONResponse:
+    request: Request,
+    new_expense_date: Annotated[str, Form()],
+    new_expense_seller: Annotated[str, Form()],
+    new_expense_total: Annotated[float, Form()],
+) -> Response:
     """Add new expense endpoint."""
     if application is None:
         return JSONResponse("")
     date = f"{new_expense_date}:{UTC}"
-    application.repository.add_expense(
-        Expense(
-            datetime.strptime(date, "%Y-%m-%d:%z"),
-            new_expense_seller,
-            new_expense_total,
-        )
+    expense = Expense(
+        datetime.strptime(date, "%Y-%m-%d:%Z").replace(tzinfo=UTC),
+        new_expense_seller,
+        new_expense_total,
     )
-    return JSONResponse("")
+    application.repository.add_expense(expense)
+    return templates.TemplateResponse(
+        request=request, name="expenses.html", context={"expense": expense}
+    )
 
 
 @app.get("/")
