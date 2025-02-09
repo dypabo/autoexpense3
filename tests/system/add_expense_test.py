80bd98dbe1b746c6646901ba41adad8c72f24872
Jason Bouchard <jason@dypabo.com>
2025-02-09 14:52:55 -0500

    refactor: tests/system/add_expense_test.py

--- tests/system/add_expense_test.py
+++ tests/system/add_expense_test.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import pytest
 from faker import Faker
+from playwright.sync_api import Locator
 from playwright.sync_api import Page
 from playwright.sync_api import expect
 
@@ -35,20 +36,44 @@ def test_page_has_new_expense_form(homepage: Page, field: str) -> None:
     expect(homepage.locator(f"#{field}")).to_be_visible(timeout=1)
 
 
-def test_add_expense_are_in_repository(
+def add_expense_to_page(page: Page, expense: Expense) -> None:
+    page.locator(f"#{DATE}").fill(expense.timestamp.strftime(DATE_FORMAT))
+    page.locator(f"#{SELLER}").fill(expense.seller)
+    page.locator(f"#{TOTAL}").fill(str(expense.total))
+    page.locator(f"#{BUTTON}").click()
+
+
+def build_expense_from_expense_line(expense: Locator) -> Expense:
+    date_str = expense.locator(".date").first.text_content()
+    total_str = expense.locator(".total").first.text_content()
+    seller_str = expense.locator(".seller").first.text_content()
+    if date_str is None or total_str is None or seller_str is None:
+        raise ValueError
+    date = datetime.strptime(date_str, "%Y-%m-%d").replace(tzinfo=UTC)
+    total = float(total_str.strip("$"))
+    return Expense(timestamp=date, seller=seller_str, total=total)
+
+
+def get_expense_from_page(page: Page, expense: Expense) -> Expense:
+    for expense_line in page.locator(".expense").all():
+        current_expense = build_expense_from_expense_line(expense_line)
+        if current_expense == expense:
+            return current_expense
+    raise ValueError
+
+
+def expense_in_page(page: Page, expense: Expense) -> bool:
+    try:
+        get_expense_from_page(page, expense)
+    except ValueError:
+        return False
+    return True
+
+
+def test_added_expense_are_in_repository(
     homepage: Page, application: Application, expense: Expense
 ) -> None:
     number_of_expense = len(application.repository.get_expenses())
-    homepage.locator(f"#{DATE}").fill(expense.timestamp.strftime(DATE_FORMAT))
-    homepage.locator(f"#{SELLER}").fill(expense.seller)
-    homepage.locator(f"#{TOTAL}").fill(str(expense.total))
-    homepage.locator(f"#{BUTTON}").click()
-
+    add_expense_to_page(homepage, expense)
     assert homepage.locator(".expense").count() == number_of_expense + 1
-    expect(homepage.locator(".expense .date").last).to_have_text(
-        expense.timestamp.strftime("%Y-%m-%d")
-    )
-    expect(homepage.locator(".expense .seller").last).to_have_text(expense.seller)
-    expect(homepage.locator(".expense .total").last).to_have_text(
-        f"{expense.total:.02f}$"
-    )
+    assert expense_in_page(homepage, expense)
