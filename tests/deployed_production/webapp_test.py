3db7b164c38bc6150337db74cf960ea188a03a19
Jason Bouchard <jason@dypabo.com>
2025-02-12 21:55:27 -0500

    build: try to see if github action can reac linode web application

--- tests/deployed_production/webapp_test.py
+++ tests/deployed_production/webapp_test.py
@@ -1,3 +1,4 @@
+import requests
 from autoexpense3.web_app.constants import APP_NAME
 from autoexpense3.web_app.constants import APP_URL
 from tests.utilities_html import get_webpage_title
@@ -9,4 +10,6 @@ def test_homepage_url_is_https() -> None:
 
 
 def test_homepage_have_appname_in_title(homepage_content: str) -> None:
+    print(requests.get("https://google.ca"))
+    print(requests.get(APP_URL))
     assert APP_NAME in get_webpage_title(homepage_content)
