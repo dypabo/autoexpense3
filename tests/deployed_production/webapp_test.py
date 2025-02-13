89bfa4440e0017d793db741c055a00cd161a11eb
Jason Bouchard <jason@dypabo.com>
2025-02-12 21:58:12 -0500

    build: add sleep to let webapp server to restart

--- tests/deployed_production/webapp_test.py
+++ tests/deployed_production/webapp_test.py
@@ -1,4 +1,3 @@
-import requests
 from autoexpense3.web_app.constants import APP_NAME
 from autoexpense3.web_app.constants import APP_URL
 from tests.utilities_html import get_webpage_title
@@ -10,6 +9,4 @@ def test_homepage_url_is_https() -> None:
 
 
 def test_homepage_have_appname_in_title(homepage_content: str) -> None:
-    print(requests.get("https://google.ca"))
-    print(requests.get(APP_URL))
     assert APP_NAME in get_webpage_title(homepage_content)
