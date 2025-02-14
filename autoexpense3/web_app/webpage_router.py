91eb406351807ec223c46f59ecfdb42f8cabf41a
Jason Bouchard <jason@dypabo.com>
2025-02-14 00:42:35 -0500

    feat: move expense report to /user url. homepage redirect to /user for now

--- autoexpense3/web_app/webpage_router.py
+++ autoexpense3/web_app/webpage_router.py
@@ -4,7 +4,7 @@ from os import environ
 
 from fastapi import APIRouter
 from fastapi.requests import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 from autoexpense3.web_app.application import Application
 from autoexpense3.web_app.constants import APP_NAME
@@ -16,8 +16,14 @@ def build_webpage_router(application: Application) -> APIRouter:
     webpage_router = APIRouter()
 
     @webpage_router.get("/")
-    def homepage(request: Request) -> HTMLResponse:
+    def homepage(request: Request) -> RedirectResponse:
         """Homepage route request processor."""
+        _ = request
+        return RedirectResponse("/user")
+
+    @webpage_router.get("/user")
+    def user(request: Request) -> HTMLResponse:
+        """User route request processor."""
         expenses = application.repository.get_expenses()
         app_url = "http://127.0.0.1:8000"
         if environ.get("GITHUB") or not environ.get("PYTEST_VERSION"):
@@ -34,5 +40,6 @@ def build_webpage_router(application: Application) -> APIRouter:
             context=context,
         )
 
+    _ = user
     _ = homepage
     return webpage_router
