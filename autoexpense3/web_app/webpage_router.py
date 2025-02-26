cc83e31de00126ee14e8db7b1a2a4611e7247406
Jason Bouchard <jason@dypabo.com>
2025-02-25 21:39:38 -0500

    feat: add register/login link and pages

--- autoexpense3/web_app/webpage_router.py
+++ autoexpense3/web_app/webpage_router.py
@@ -1,11 +1,15 @@
 from datetime import UTC
 from datetime import datetime
-from os import environ
+from typing import Annotated
+from uuid import uuid4
 
 from fastapi import APIRouter
+from fastapi import Form
 from fastapi.requests import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import Response
 
+from autoexpense3.models.user import User
 from autoexpense3.web_app.application import Application
 from autoexpense3.web_app.constants import APP_NAME
 from autoexpense3.web_app.constants import APP_URL
@@ -31,12 +35,9 @@ def build_webpage_router(application: Application) -> APIRouter:
     def user(request: Request) -> HTMLResponse:
         """User route request processor."""
         expenses = application.repository.get_expenses()
-        app_url = "http://127.0.0.1:8000"
-        if environ.get("GITHUB") or not environ.get("PYTEST_VERSION"):
-            app_url = APP_URL
         context = {
             "app_name": APP_NAME,
-            "app_url": app_url,
+            "app_url": APP_URL,
             "expenses": expenses,
             "today": datetime.now(tz=UTC),
         }
@@ -46,6 +47,63 @@ def build_webpage_router(application: Application) -> APIRouter:
             context=context,
         )
 
+    @webpage_router.get("/login")
+    def login(request: Request) -> HTMLResponse:
+        """User route request processor."""
+        context = {
+            "app_name": APP_NAME,
+            "app_url": APP_URL,
+        }
+        return application.templates.TemplateResponse(
+            request=request,
+            name="login.html",
+            context=context,
+        )
+
+    @webpage_router.post("/login")
+    def login_user(
+        request: Request,
+        email: Annotated[str, Form()],
+        password: Annotated[str, Form()],
+    ) -> Response:
+        """User route request processor."""
+        _ = request
+        _ = email
+        _ = password
+        response = JSONResponse({})
+        response.headers["HX-Redirect"] = "/user"
+        return response
+
+    @webpage_router.get("/register")
+    def register(request: Request) -> HTMLResponse:
+        """User route request processor."""
+        context = {
+            "app_name": APP_NAME,
+            "app_url": APP_URL,
+        }
+        return application.templates.TemplateResponse(
+            request=request,
+            name="register.html",
+            context=context,
+        )
+
+    @webpage_router.post("/register")
+    def register_new_user(
+        request: Request,
+        email: Annotated[str, Form()],
+        password: Annotated[str, Form()],
+    ) -> Response:
+        """User route request processor."""
+        _ = request
+        application.repository.add_user(User(uuid4(), email, password))
+        response = JSONResponse({})
+        response.headers["HX-Redirect"] = "/login"
+        return response
+
     _ = user
     _ = homepage
+    _ = register
+    _ = register_new_user
+    _ = login
+    _ = login_user
     return webpage_router
--- tests/deployed_stagging/homepage_test.py
+++ tests/deployed_stagging/homepage_test.py
@@ -6,3 +6,11 @@ from autoexpense3.web_app.constants import APP_NAME
 
 def test_app_name_in_homepage_title(homepage: Page) -> None:
     expect(homepage).to_have_title(APP_NAME)
+
+
+def test_register_button_present_in_page(homepage: Page) -> None:
+    expect(homepage.locator("#register")).to_have_text("Register")
+
+
+def test_login_button_present_in_page(homepage: Page) -> None:
+    expect(homepage.locator("#login")).to_have_text("Login")
